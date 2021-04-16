caa6d0c0fe36ef674487f035f166361a86dbe5db
sandhyavisu <81370245+sandhyavisu@users.noreply.github.com>
2021-04-16 23:13:28 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+%%writefile app.py
+import streamlit as st
+import sklearn
+import joblib
+model = joblib.load('Email_Class')
+st.title('viral/limited Classifier')
+ip = st.text_input('Enter your message')
+op = model.predict([ip])
+if st.button('Predict'):
+  st.title(op[0])
